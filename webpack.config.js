3df67357d7d7ec837d814906de5b6bf17163b801
Troels Ugilt Jensen <tujensen@gmail.com>
2019-03-26 12:57:45 +0100

    KON-64: Added login

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
+    .addEntry('login', './assets/css/login.css')
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
