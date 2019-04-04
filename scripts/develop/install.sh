8200e30d1d3681a3c7e5972764a9089fd870c563
Jesper Kristensen <cableman@linuxdev.dk>
2019-04-04 15:01:55 +0200

    KON-74: Updated install script to exit pretty

--- scripts/develop/install.sh
+++ scripts/develop/install.sh
@@ -4,7 +4,12 @@ normal=$(tput sgr0)
 
 dir=$(cd $(dirname "${BASH_SOURCE[0]}")/../../ && pwd)
 
-mkdir -p $dir/packages/kontrolgruppen || exit
+if [ -d $dir/packages/kontrolgruppen ]; then
+    (>&2 echo Package exits)
+    exit
+fi
+
+mkdir -p $dir/packages/kontrolgruppen
 
 git clone --branch=develop https://github.com/aakb/kontrolgruppen-core-bundle.git $dir/packages/kontrolgruppen/core-bundle
 
