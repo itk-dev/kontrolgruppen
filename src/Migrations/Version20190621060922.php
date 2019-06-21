57dc901cf0a3dafaedb6763a8de6d2841ae697f8
Troels Ugilt Jensen <tujensen@gmail.com>
2019-06-21 08:21:29 +0200

    KON-11: Cleaned up dates

--- src/Migrations/Version20190621060922.php
+++ src/Migrations/Version20190621060922.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190612132727 extends AbstractMigration
+final class Version20190621060922 extends AbstractMigration
 {
     public function getDescription() : string
     {
