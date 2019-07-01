6aa694b95bfa74dbda449d92f6db88a7ea906729
Troels Ugilt Jensen <tujensen@gmail.com>
2019-07-01 13:21:28 +0200

    KON-130: Applied coding-standards

--- src/Migrations/Version20190701092452.php
+++ src/Migrations/Version20190701092452.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,23 +20,23 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190701092452 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE client ADD self_employed TINYINT(1) DEFAULT NULL, ADD works_in_major_company TINYINT(1) DEFAULT NULL, ADD not_employed TINYINT(1) DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE client DROP self_employed, DROP works_in_major_company, DROP not_employed');
     }
