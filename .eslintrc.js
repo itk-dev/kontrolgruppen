801b1e8cdfec68aa919c09a635fab50b299993bf
Mikkel Ricky <rimi@aarhus.dk>
2019-07-01 23:17:40 +0200

    KON-174: Added and applied coding standards

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "root": true,
+    "extends": "standard",
+    "rules": {
+        "indent": ["error", 4],
+        // Party like it's 1999!
+        "semi": ["error", "always"]
+    },
+    "env": {
+        "jquery": true
+    }
+};
