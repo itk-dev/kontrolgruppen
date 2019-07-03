856baef757518414de3a74e6f54d918acf72032c
Troels Ugilt Jensen <tujensen@gmail.com>
2019-07-03 10:12:33 +0200

    KON-140: Applied coding standards

--- src/Migrations/Version20190702131545.php
+++ src/Migrations/Version20190702131545.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,25 +20,25 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190702131545 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE car (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, registration_number VARCHAR(255) NOT NULL, shared_ownership TINYINT(1) DEFAULT NULL, notes LONGTEXT DEFAULT NULL, INDEX IDX_773DE69D19EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ENCRYPTED = YES');
         $this->addSql('ALTER TABLE car ADD CONSTRAINT FK_773DE69D19EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
         $this->addSql('ALTER TABLE client ADD has_drivers_license TINYINT(1) DEFAULT NULL, ADD has_car TINYINT(1) DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE car');
         $this->addSql('ALTER TABLE client DROP has_drivers_license, DROP has_car');
