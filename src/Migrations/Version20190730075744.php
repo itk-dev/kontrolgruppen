e9c5bf75a9ebbfa4c869cdccf29eee201722d3b2
Troels Ugilt Jensen <tujensen@gmail.com>
2019-07-30 10:27:26 +0200

    KON-188: Added migration and fixtures

---
+++ src/Migrations/Version20190730075744.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190730075744 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE process_type_channel (process_type_id INT NOT NULL, channel_id INT NOT NULL, INDEX IDX_E066B4838D345646 (process_type_id), INDEX IDX_E066B48372F5A1AA (channel_id), PRIMARY KEY(process_type_id, channel_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB ENCRYPTED = YES');
+        $this->addSql('ALTER TABLE process_type_channel ADD CONSTRAINT FK_E066B4838D345646 FOREIGN KEY (process_type_id) REFERENCES process_type (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE process_type_channel ADD CONSTRAINT FK_E066B48372F5A1AA FOREIGN KEY (channel_id) REFERENCES channel (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE process_type_channel');
+    }
+}
