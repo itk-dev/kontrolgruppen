8cd6df9191dafdf1cc804569b206d15c806279f4
Lars Steen <lats@aarhus.dk>
2019-08-13 13:17:06 +0200

    KON-209: Applying coding standards

--- src/Migrations/Version20190813091713.php
+++ src/Migrations/Version20190813091713.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,15 +20,15 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190813091713 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE process_type ADD default_process_status_id INT NOT NULL, ADD default_process_status_on_empty_case_worker_id INT NOT NULL');
         $this->addSql('ALTER TABLE process_type ADD CONSTRAINT FK_6337851C43E7BCA FOREIGN KEY (default_process_status_id) REFERENCES process_status (id)');
@@ -29,10 +37,10 @@ final class Version20190813091713 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_6337851C52436242 ON process_type (default_process_status_on_empty_case_worker_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE process_type DROP FOREIGN KEY FK_6337851C43E7BCA');
         $this->addSql('ALTER TABLE process_type DROP FOREIGN KEY FK_6337851C52436242');
