df84d043646d78c70f39826538fd413f72eba384
Lars Steen <lats@aarhus.dk>
2019-08-14 09:01:05 +0200

    KON-217: Adding js for only showing process status changes in journal entry page

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ Encore
     .addEntry('cars', './bundles/kontrolgruppen-core-bundle/Resources/assets/js/cars.js')
     .addEntry('processReport', './bundles/kontrolgruppen-core-bundle/Resources/assets/js/processReport.js')
     .addEntry('export', './bundles/kontrolgruppen-core-bundle/Resources/assets/js/export.js')
+    .addEntry('processStatusChangeToggle', './bundles/kontrolgruppen-core-bundle/Resources/assets/js/processStatusChangeToggle.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
