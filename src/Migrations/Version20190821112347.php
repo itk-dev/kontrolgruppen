04df6a330e06505e03459a9ddbe2bdffddd3980f
Lars Steen <lats@aarhus.dk>
2019-08-21 14:38:16 +0200

    KON-189: Adding account to base economy entry

---
+++ src/Migrations/Version20190821112347.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190821112347 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_by VARCHAR(255) DEFAULT NULL, updated_by VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE economy_entry ADD account_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE economy_entry ADD CONSTRAINT FK_EE7979A29B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
+        $this->addSql('CREATE INDEX IDX_EE7979A29B6B5FBA ON economy_entry (account_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE economy_entry DROP FOREIGN KEY FK_EE7979A29B6B5FBA');
+        $this->addSql('DROP TABLE account');
+        $this->addSql('DROP INDEX IDX_EE7979A29B6B5FBA ON economy_entry');
+        $this->addSql('ALTER TABLE economy_entry DROP account_id');
+    }
+}
