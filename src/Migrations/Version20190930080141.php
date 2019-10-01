ec2780397e356571e35f2441d22c8b631b814570
Lars Steen <lats@aarhus.dk>
2019-10-01 13:07:17 +0200

    KON-245: Applying coding standards

--- src/Migrations/Version20190930080141.php
+++ src/Migrations/Version20190930080141.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,24 +20,24 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190930080141 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE process_log_entry DROP FOREIGN KEY FK_911634C57EC2F574');
         $this->addSql('ALTER TABLE process_log_entry ADD CONSTRAINT FK_911634C57EC2F574 FOREIGN KEY (process_id) REFERENCES process (id) ON DELETE CASCADE');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE process_log_entry DROP FOREIGN KEY FK_911634C57EC2F574');
         $this->addSql('ALTER TABLE process_log_entry ADD CONSTRAINT FK_911634C57EC2F574 FOREIGN KEY (process_id) REFERENCES process (id)');
