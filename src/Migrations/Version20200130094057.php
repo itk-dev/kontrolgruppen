ad4c794e4446059009f3daf1d1b94d482e2f145e
Troels Ugilt Jensen <tujensen@gmail.com>
2020-01-30 13:45:15 +0100

    KON-321: Added migration

--- src/Migrations/Version20200130094057.php
+++ src/Migrations/Version20200130094057.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,24 +20,24 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200130094057 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user_settings DROP INDEX UNIQ_5C844C5A76ED395, ADD INDEX IDX_5C844C5A76ED395 (user_id)');
         $this->addSql('ALTER TABLE user_settings ADD settings_key VARCHAR(255) NOT NULL, ADD settings_value LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', DROP process_index_sort');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user_settings DROP INDEX IDX_5C844C5A76ED395, ADD UNIQUE INDEX UNIQ_5C844C5A76ED395 (user_id)');
         $this->addSql('ALTER TABLE user_settings ADD process_index_sort VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, DROP settings_key, DROP settings_value');
