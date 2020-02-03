1962076d2e4f0084074abe30e054ee803f03028d
Troels Ugilt Jensen <tujensen@gmail.com>
2020-02-03 11:03:05 +0100

    KON-328: Added fixtures

---
+++ src/DataFixtures/ForwardedToAuthorityFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\DataFixtures;
+
+use Kontrolgruppen\CoreBundle\Entity\ForwardedToAuthority;
+
+/**
+ * Class ForwardedToAuthorityFixture
+ */
+class ForwardedToAuthorityFixture extends AbstractFixture
+{
+    protected $class = ForwardedToAuthority::class;
+}
