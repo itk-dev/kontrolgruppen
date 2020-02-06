0b5081b5de36f8e7147479e601b475a9b40375ae
Troels Ugilt Jensen <tujensen@gmail.com>
2020-02-06 14:03:33 +0100

    KON-332: Added tests

---
+++ tests/Kontrolgruppen/CoreBundle/Service/EconomyTest.php
@@ -0,0 +1,99 @@
+<?php
+
+
+namespace App\Tests\Kontrolgruppen\CoreBundle\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Kontrolgruppen\CoreBundle\DBAL\Types\RevenueTypeEnumType;
+use Kontrolgruppen\CoreBundle\Entity\Process;
+use Kontrolgruppen\CoreBundle\Entity\RevenueEntry;
+use Kontrolgruppen\CoreBundle\Entity\Service;
+use Kontrolgruppen\CoreBundle\Repository\EconomyEntryRepository;
+use Kontrolgruppen\CoreBundle\Repository\LockedNetValueRepository;
+use Kontrolgruppen\CoreBundle\Repository\ProcessRepository;
+use Kontrolgruppen\CoreBundle\Repository\RevenueEntryRepository;
+use Kontrolgruppen\CoreBundle\Repository\ServiceRepository;
+use Kontrolgruppen\CoreBundle\Service\EconomyService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class EconomyTest.
+ */
+class EconomyTest extends KernelTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+    public function testCalculateFunction() {
+        // Get special container that allows fetching private services
+        $container = self::$container;
+
+        // Get services.
+        $processRepository = $container->get(ProcessRepository::class);
+        $economyService = $container->get(EconomyService::class);
+        $revenueEntryRepository = $container->get(RevenueEntryRepository::class);
+        $serviceRepository = $container->get(ServiceRepository::class);
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $process = $processRepository->findOneById(1);
+
+        $revenueEntries = $revenueEntryRepository->findBy([
+            'process' => $process,
+        ]);
+
+        $result = $economyService->calculateRevenue($process);
+
+        $expected = [
+            'entries' => $revenueEntries,
+            'repaymentSum' => 2100.0,
+            'repaymentSums' => [
+                'Kontanthjælp' => [
+                    'netPercentage' => 70.0,
+                    'sum' => 1200.0,
+                ],
+                'Sygedagpenge' => [
+                    'netPercentage' => 70.0,
+                    'sum' => 900.0,
+                ],
+            ],
+            'netRepaymentSum' => 1470.0,
+            'futureSavingsSum' =>  11900.0,
+            'futureSavingsSums' => [
+                'Kontanthjælp' => [
+                    'netPercentage' => 70.0,
+                    'sum' => 1100.0,
+                ],
+                'Sygedagpenge' => [
+                    'netPercentage' => 70.0,
+                    'sum' => 10800.0,
+                ],
+            ],
+            'netFutureSavingsSum' => 8330.0,
+            'netCollectiveSum' => 9800.0,
+            'collectiveSum' => 14000.0,
+        ];
+
+        $this->assertEquals($expected, $result, 'Not expected results');
+
+        $service = $serviceRepository->findOneBy(['name' => 'Kontanthjælp']);
+
+        $newRevenueEntry = new RevenueEntry();
+        $newRevenueEntry->setService($service);
+        $newRevenueEntry->setProcess($process);
+        $newRevenueEntry->setType(RevenueTypeEnumType::REPAYMENT);
+        $newRevenueEntry->setAmount(500);
+
+        $entityManager->persist($newRevenueEntry);
+        $entityManager->flush();
+
+        $result = $economyService->calculateRevenue($process);
+
+        $this->assertEquals(2600.0, $result['repaymentSum'], 'Repayment does not match.');
+        $this->assertEquals(14500.0, $result['collectiveSum'], 'Repayment does not match.');
+    }
+}
