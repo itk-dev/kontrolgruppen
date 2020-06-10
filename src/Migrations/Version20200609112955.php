3900258b724fda14d850531b79b7b2d25ef7a9f9
Lars Steen <lats@aarhus.dk>
2020-06-10 12:21:34 +0200

    KON-288: Applying coding standards

--- src/Migrations/Version20200609112955.php
+++ src/Migrations/Version20200609112955.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of aakb/kontrolgruppen.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,15 +20,15 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200609112955 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE process_group (id INT AUTO_INCREMENT NOT NULL, primary_process_id INT NOT NULL, INDEX IDX_3B9C693440661912 (primary_process_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ENCRYPTED = YES');
         $this->addSql('CREATE TABLE process_group_process (process_group_id INT NOT NULL, process_id INT NOT NULL, INDEX IDX_51A9F2F58A587C6 (process_group_id), INDEX IDX_51A9F2F57EC2F574 (process_id), PRIMARY KEY(process_group_id, process_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ENCRYPTED = YES');
@@ -29,10 +37,10 @@ final class Version20200609112955 extends AbstractMigration
         $this->addSql('ALTER TABLE process_group_process ADD CONSTRAINT FK_51A9F2F57EC2F574 FOREIGN KEY (process_id) REFERENCES process (id) ON DELETE CASCADE');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE process_group_process DROP FOREIGN KEY FK_51A9F2F58A587C6');
         $this->addSql('DROP TABLE process_group');
