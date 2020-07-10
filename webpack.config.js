34107e71514196d5e35ae5d4c287f430b3863777
Lars Steen <lats@aarhus.dk>
2020-07-10 09:48:41 +0200

    KON-365: Adding asset entry to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ Encore
     .addEntry('revenueForm', './vendor/kontrolgruppen/core-bundle/Resources/assets/js/revenueForm.js')
     .addEntry('dashboard', './vendor/kontrolgruppen/core-bundle/Resources/assets/js/dashboard.js')
     .addEntry('journalEntryPreventDoubleSubmission', './vendor/kontrolgruppen/core-bundle/Resources/assets/js/journalEntryPreventDoubleSubmission.js')
+    .addEntry('processFormCprSearch', './vendor/kontrolgruppen/core-bundle/Resources/assets/js/processFormCprSearch.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
